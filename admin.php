aabf6731c3746a0df14ef75833b24787427eea1e
timxx21 <112845568+timxx21@users.noreply.github.com>
2023-05-29 09:11:41 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -24,7 +24,10 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
     empty($_SERVER['PHP_AUTH_PW']) ||
     $_SERVER['PHP_AUTH_USER'] != 'admin' ||
     md5($_SERVER['PHP_AUTH_PW']) != md5('123')) {
-  authorize();
+  header('HTTP/1.1 401 Unanthorized');
+  header('WWW-Authenticate: Basic realm="My site"');
+  print('<h1>401 Требуется авторизация</h1>');
+  exit();
 }
 $user = 'u52808';
 $pass = '9337244';
