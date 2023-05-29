db3d664586280d3554bfe8deb954599f96f4b098
timxx21 <112845568+timxx21@users.noreply.github.com>
2023-05-29 09:09:50 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -21,7 +21,7 @@ function authorize() {
 // PHP хранит логин и пароль в суперглобальном массиве $_SERVER.
 // Подробнее см. стр. 26 и 99 в учебном пособии Веб-программирование и веб-сервисы.
 if (empty($_SERVER['PHP_AUTH_USER']) ||
-    empty($_SERVER['PHP_AUTH_PW']) {
+    empty($_SERVER['PHP_AUTH_PW'])) {
   authorize();
 }
 $user = 'u52808';
